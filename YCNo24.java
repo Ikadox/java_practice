fafdddc166718fbcc8f7d58361b8d27e465e16c7
Ikadox <ikado@ikadonoMacBook-Pro.local>
2020-02-08 15:22:53 +0900

    候補リストから削除するアルゴリズムに変更

--- YCNo24.java
+++ YCNo24.java
@@ -5,77 +5,61 @@ import java.util.List;
 import java.util.Scanner;
 
 public class YCNo24{
+    private static List<String> candidateList = new ArrayList<>();
     public static void main(final String[] args) {
-        try {
-            final int turnNumber = createTurnNumber();
-            List<String> nonCandidateList = ioNumberList(turnNumber);
-            outputAnswer(nonCandidateList);
+        final int formatMaxNumber = 9;
+        final int formatMinNumber = 0;
+        formatArray(formatMaxNumber, formatMinNumber) ;
+
+        try(Scanner scan = new Scanner(System.in)) {
+            int turnNumber  = 0;
+            do{
+                turnNumber = Integer.parseInt(scan.nextLine());
+            }while(!isTurnNumber(turnNumber));
+
+            for(int i=0; i<turnNumber; i++){
+                String[] inputNumberArray = scan.nextLine().split(" ");
+                ioNumberList(inputNumberArray);
+            }
+            candidateList.forEach(a -> System.out.println(a));
+            scan.close();
         } catch (final InputMismatchException e) {
             System.out.println("you must input integer.");
         } catch (final ArrayIndexOutOfBoundsException e) {
             System.out.println("you must input 4 integers");
         }
     }
-    /**
-     * ターン数の入力を受け付け、登録する
-     * 登録される数値：Nの範囲は 2 <= N <= 6 である
-     * @return ターン数
-     */
-    private static int createTurnNumber(){
-        int turnNumber = 0;
-        Scanner scan = new Scanner(System.in);
-        boolean isTurnNumberRange = false;
-            while (!isTurnNumberRange) {
-                turnNumber = scan.nextInt();
-                if (turnNumber >= 2 && turnNumber <= 6) {
-                    isTurnNumberRange = true;
-                }
-            }
-        return turnNumber;
-    }
-    /**
-     * NOとなった数値を nonCandidateList に入れて、
-     * YESとなった数値はその数値以外を nonCandidateList に入れる
-     * @param turnNumber
-     * @return 正答以外の候補リスト
-     */
-    private static List<String> ioNumberList (int turnNumber){
-        List<String> nonCandidateList = new ArrayList<>(); 
 
-        Scanner scan = new Scanner(System.in);
-        for (int i = 0; i < turnNumber; i++) {
-            String[] inputNumberArray = scan.nextLine().split(" ");
-            
-            if(inputNumberArray[(inputNumberArray.length)-1].equals("NO")){
-                for(int j=0; j<inputNumberArray.length; j++){
-                    nonCandidateList.add(inputNumberArray[j]);
-                }
-            }else{
-                for(int j=0; j<inputNumberArray.length-1; j++){
-                    List<String> checkBox = Arrays.asList(inputNumberArray);
-                    for(int m=0; m<10; m++){
-                        Integer number  = m;
-                        if(!checkBox.contains(number.toString())){
-                            nonCandidateList.add(number.toString());
-                        }
-                    }
-                }
-            }
+    private static void formatArray(int formatMaxNumber, int formatMinNumber){
+        for(; formatMinNumber<=formatMaxNumber; formatMinNumber++){
+            candidateList.add(Integer.toString(formatMinNumber));
         }
-        return nonCandidateList;
-        
+    }
+
+    /**
+     * ターン数の範囲確認
+     * 許容される数値の範囲N： 2 <= N <= 6 である
+     * @return boolean
+     */
+    private static boolean isTurnNumber(int turnNumber){
+        return (turnNumber >= 2 && turnNumber <= 6)? true : false;
     }
     /**
-     * nonCandidateList にない数値を探して出力する
-     * @param nonCandidateList
+     * 入力された数値がNoの場合、candidateListから削除する
+     * 入力された数値がYesの場合、inputNumberListに含まれていないcandidateListの値を削除する
+     * @param inputNumberArray
      */
-    private static void outputAnswer(List<String> nonCandidateList){
-        for(int i=0; i<10; i++){
-            Integer checkedNumber  = i;
-            if(!nonCandidateList.contains(checkedNumber.toString())){
-                    System.out.println(i);
-                    return;
+    private static void ioNumberList (String[] inputNumberArray){
+        List<String> inputNumberList = Arrays.asList(inputNumberArray);
+        List<String> nonCandidateIndexList = new ArrayList<>();
+        if(inputNumberArray[(inputNumberArray.length)-1].equals("NO")){
+            inputNumberList.forEach(a -> candidateList.remove(a));
+        }else{
+            for(String a: candidateList){
+                if(!inputNumberList.contains(a))
+                    nonCandidateIndexList.add(a);
             }
         }
+        nonCandidateIndexList.forEach(a -> candidateList.remove(a));
     }
 }
